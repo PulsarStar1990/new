9f70f0ed205430438017ecf30e103eecfa270cee
hate4u <dreamston18@gmail.com>
2017-02-05 12:43:25 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <title>Саша-Студент</title>
+        <style type="text/css">
+        a, a:hover {
+        color: #bbb;
+        text-decoration: none;
+        </style>
+    </head>
+    <body>
+    <h1>Имя - Александр.</h1>
+    <h2>Возраст - 22 года.</h2>
+    <h3>email - <a href="dreamston18@gmail.com">dreamston18@gmail.com</a></h3>
+    <h4>Город - Самара</h4>
+    <h5>О себе - будущий PHP разработчик</h5>
+    </body>
+</html>
