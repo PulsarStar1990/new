8392916a84afd21981ad0dee8bfd5e4b709aed54
hate4u <dreamston18@gmail.com>
2017-02-13 20:05:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+	$x = rand(0, 100);
+	$first_number = 1;
+	$second_number = 1;
+	if ($x > $first_number) {
+		echo "задуманное число НЕ входит в числовой ряд";
+		# Условие для большего числа.
+	}
+	elseif ($x ==+ $first_number) {
+		echo "задуманное число входит в числовой ряд";
+		# Условие для равенства.
+	}
+	do {	
+		$second_number = $x;
+		$x = $x + $first_number;
+		$first_number = $second_number;
+	} 
+	while ($x <= $first_number);
+echo "<br />".$x."<br />";
+echo $first_number."<br />";
+echo $second_number."<br />";
+ ?>
